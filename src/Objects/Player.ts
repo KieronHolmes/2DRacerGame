ea161c9e09b0a5b8cd7224a3853003ddbd7e6076
KieronHolmes <kieron@kbho.co.uk>
2022-01-18 12:28:58 +0000

    Added levels and removed inertia upon collision

--- src/Objects/Player.ts
+++ src/Objects/Player.ts
@@ -2,6 +2,7 @@ import MatterEntity from "./MatterEntity";
 
 export class Player extends MatterEntity {
   private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+  private hasCrashed: boolean = false;
 
   constructor(scene: Phaser.Scene,x: number,y: number,texture: string, playerAngle: number) {
     super(scene, x, y, texture);
@@ -12,28 +13,35 @@ export class Player extends MatterEntity {
 
     this.cursors = this.scene.input.keyboard.createCursorKeys();
     this.scene.add.existing(this);
+    this.setFixedRotation();
   }
 
   update(): void {
+    this.setOnCollide(function() {
+      this.hasCrashed = true;
+      console.log("Crashed");
+    });
     // handle input
-    if (this.cursors.up.isDown) {
-      this.setVelocity(0,-3);
-      this.setAngle(0);
-    }
-    else if (this.cursors.down.isDown) {
-      this.setVelocity(0,3);
-      this.setAngle(180);
-    }
-    else if (this.cursors.left.isDown) {
-      this.setVelocity(-3,0);
-      this.setAngle(270);
-    }
-    else if (this.cursors.right.isDown) {
-      this.setVelocity(3,0);
-      this.setAngle(90);
-    }
-    else {
-      this.setVelocity(0,0);
+    if(!this.hasCrashed) {
+      if (this.cursors.up.isDown) {
+        this.setVelocity(0,-3);
+        this.setAngle(0);
+      }
+      else if (this.cursors.down.isDown) {
+        this.setVelocity(0,3);
+        this.setAngle(180);
+      }
+      else if (this.cursors.left.isDown) {
+        this.setVelocity(-3,0);
+        this.setAngle(270);
+      }
+      else if (this.cursors.right.isDown) {
+        this.setVelocity(3,0);
+        this.setAngle(90);
+      }
+      else {
+        this.setVelocity(0,0);
+      }
     }
   }
 }
